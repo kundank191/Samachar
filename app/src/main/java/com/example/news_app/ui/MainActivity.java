7553ab64d0b924c534cce868558f99ad0a7ea0e7
Kundan <kundank191@gmail.com>
2018-08-20 23:58:47 +0530

    git init

---
+++ app/src/main/java/com/example/news_app/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.news_app.ui;
+
+import android.os.Bundle;
+
+import com.example.news_app.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container_view,new NewsFragment())
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/example/news_app/ui/ViewModel.java
@@ -0,0 +1,7 @@
+package com.example.news_app.ui;
+
+/**
+ * Created by Kundan on 20-08-2018.
+ */
+public class ViewModel extends androidx.lifecycle.ViewModel {
+}

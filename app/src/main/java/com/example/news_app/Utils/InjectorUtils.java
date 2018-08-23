34a7c9f07bf901361d9df8e444ab5638fbfb5f00
Kundan <kundank191@gmail.com>
2018-08-23 21:59:06 +0530

    feat : create view model and injector utils class

---
+++ app/src/main/java/com/example/news_app/Utils/InjectorUtils.java
@@ -0,0 +1,7 @@
+package com.example.news_app.Utils;
+
+/**
+ * Created by Kundan on 23-08-2018.
+ */
+public class InjectorUtils {
+}
--- app/src/main/java/com/example/news_app/ui/ViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.news_app.ui;
-
-/**
- * Created by Kundan on 20-08-2018.
- */
-public class ViewModel extends android.arch.lifecycle.ViewModel {
-}

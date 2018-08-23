d89e00b2cffde6303e688f3870a3705c600b1dd4
Kundan <kundank191@gmail.com>
2018-08-23 21:57:41 +0530

    feat : add repository to view model

--- app/src/main/java/com/example/news_app/ui/ArticleViewModel.java
+++ app/src/main/java/com/example/news_app/ui/ArticleViewModel.java
@@ -2,9 +2,12 @@ package com.example.news_app.ui;
 
 import android.arch.lifecycle.LiveData;
 
-import com.example.news_app.data.NetworkResponse;
+import com.example.news_app.data.Network.NetworkResponse;
+import com.example.news_app.data.NewsRepository;
 
 import java.util.List;
+
+import javax.inject.Inject;
 
 /**
  * Created by Kundan on 20-08-2018.
@@ -12,8 +15,14 @@ import java.util.List;
 public class ArticleViewModel extends android.arch.lifecycle.ViewModel {
 
     private LiveData<List<NetworkResponse.Article>> article;
+    private NewsRepository newsRepository;
 
-    public LiveData<List<NetworkResponse.Article>> getArticle() {
+    @Inject // Parameter provided by dagger 2
+    public ArticleViewModel(NewsRepository newsRepository){
+        this.newsRepository = newsRepository;
+    }
+
+    public LiveData<List<NetworkResponse.Article>> getArticles() {
         return article;
     }
 

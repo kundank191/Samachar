7c19383624b5f4342bbf58c2f05fdf61fcd1ac8f
Kundan <kundank191@gmail.com>
2018-08-26 15:53:55 +0530

    feat : add glide dependency and module

---
+++ app/src/main/java/com/example/news_app/Utils/GlideModule.java
@@ -0,0 +1,10 @@
+package com.example.news_app.Utils;
+
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by Kundan on 26-08-2018.
+ */
+@com.bumptech.glide.annotation.GlideModule
+public class GlideModule extends AppGlideModule {
+}
